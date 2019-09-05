536cff9996e001a07ef5c9379eda0e733f9769e6
Phoumrin Tan <phoumrint@sbcglobal.net>
2019-09-04 20:46:14 -0700

    Fix runtime issues.

--- boutique/src/app/app.module.ts
+++ boutique/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +19,9 @@ import { SignService } from './services/sign.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     SignService
