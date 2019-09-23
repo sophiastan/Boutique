f9cf188da63f02c11fef6b9ef1a51452e2c9a917
Phoumrin Tan <phoumrint@sbcglobal.net>
2019-09-22 23:00:34 -0700

    Fixed integration key

--- boutique/src/app/services/sign.service.ts
+++ boutique/src/app/services/sign.service.ts
@@ -8,6 +8,8 @@ export class SignService {
     baseUrl = 'https://api.na2.echosign.com';
     uploadUrl = this.baseUrl + '/api/rest/v6/transientDocuments';
     sendUrl = this.baseUrl + '/api/rest/v6/agreements';
+    integrationKey = '3AAABLblqZhCUzZTQu2MvFsn9FbiEexxLzYfCVOK9b9U2HvIBW_9OlYJ-FxZDgnMsih6024_N50ZgKujZC3kt5ccinyfceyVE';
+
 
     constructor(private http: HttpClient) {}
 
@@ -17,43 +19,20 @@ export class SignService {
         formData.append('form-data', file);
 
         // Access Token obtained from authorization code (code in the request, client id, client secret) 
-        //const accessToken = '3AAABLblqZhB2DTcTU_NFdPMsgsZJMQY8li8HISRx6PgL5GjPbOddzm34jBt4Bm9o8qmm9mDvWHlPtYwKr20o3SbTIhbPQChG';
-        const integrationKey = '3AAABLblqZhCUzZTQu2MvFsn9FbiEexxLzYfCVOK9b9U2HvIBW_9OlYJ-FxZDgnMsih6024_N50ZgKujZC3kt5ccinyfceyVE';
-
-        // Specify access token
-        // const headers = {
-        //      'Authorization': 'Bearer ' + accessToken,
-        //      'Content-Type': 'multipart/form-data'
-        //  };
-
-        const t = localStorage.getItem(integrationKey);
-
+        const contentDisposition = 'form-data; name=";File"; filename="' + file.name + '"';
         const httpOptions = {
             headers: new HttpHeaders({
-                'Authorization': 'Bearer ' + t,
-                'Content-Type': 'multipart/form-data'
+                'Authorization': 'Bearer ' + this.integrationKey
             })
         };
 
-
-        // let headers = new HttpHeaders();
-        // headers.set('Authorization', 'Bearer ' + accessToken);
-        // headers.set('Content-Type', null);
-        // headers.append('Accept', 'multipart/form-data');
-
         console.log(httpOptions.headers);
-        //console.log(headers);
-        
-        //return this.http.post(this.uploadUrl, formData, { 'headers': headers });
         return this.http.post(this.uploadUrl, formData, httpOptions);
-        console.log("upload worked!");
     }
 
     sendAgreement(documentId: string): Observable<any> {
-        const accessToken = '3AAABLblqZhB2DTcTU_NFdPMsgsZJMQY8li8HISRx6PgL5GjPbOddzm34jBt4Bm9o8qmm9mDvWHlPtYwKr20o3SbTIhbPQChG';
-
         const headers = {
-            'Authorization': 'Bearer ' + accessToken,
+            'Authorization': 'Bearer ' + this.integrationKey,
             'Content-Type': 'application/json'
         };
 
@@ -75,6 +54,5 @@ export class SignService {
         };
 
         return this.http.post(this.sendUrl, agreementInfo, {'headers': headers});
-        console.log("send file worked!");
     }
 }
