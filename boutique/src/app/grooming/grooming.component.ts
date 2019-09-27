d09a08b3eceeef60cc787517642c157de5df6edc
Sophia Tan <soptan@adobe.com>
2019-09-26 20:47:16 -0700

    got agreementInfo and agreementID

--- boutique/src/app/grooming/grooming.component.ts
+++ boutique/src/app/grooming/grooming.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SignService } from '../services/sign.service';
 import { Pet } from './pet'
-import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-grooming',
@@ -11,6 +10,7 @@ import { ThrowStmt } from '@angular/compiler';
 })
 export class GroomingComponent implements OnInit {
   pet: Pet;
+  docName: string;
   // Define default variable for selected file
   fileToUpload: File = null;
 
@@ -28,34 +28,34 @@ export class GroomingComponent implements OnInit {
     this.fileToUpload = files.item(0);
   }
 
-  // uploadFile() {
-  //   // Upload document to get document Id
-  //   this.signService.uploadFile(this.fileToUpload)
-  //     .subscribe(data => {
-  //       // Get document Id
-  //       const docId = data.transientDocumentId;
+  uploadFile() {
+    // Upload document to get document Id
+    this.signService.uploadFile(this.fileToUpload)
+      .subscribe(data => {
+        // Get document Id
+        const docId = data.transientDocumentId;
 
-  //       // Send document
-  //       this.signService.sendAgreement(docId)
-  //         .subscribe(d => {
-  //           // Get sign generated Id
-  //           const signId = d.id;
-  //         },
-  //           error => {
-  //             // Display error from send document
-  //             console.log(error);
-  //           }
-  //         );
-  //     },
-  //       error => {
-  //         // Display error from uploading document
-  //         console.log(error);
-  //       }
-  //     );
-  // }
+        // // Send document
+        // this.signService.sendAgreement(docId)
+        //   .subscribe(d => {
+        //     // Get sign generated Id
+        //     const signId = d.id;
+        //   },
+        //     error => {
+        //       // Display error from send document
+        //       console.log(error);
+        //     }
+        //   );
+      },
+        error => {
+          // Display error from uploading document
+          console.log(error);
+        }
+      );
+  }
 
-  postAgreement() {
-    this.signService.postAgreement(this.pet)
+  postAgreement(docId: string) {
+    this.signService.postAgreement(this.pet, docId)
       .subscribe(data => {
         console.log(data);
       }),
@@ -63,24 +63,29 @@ export class GroomingComponent implements OnInit {
           console.log(error);
         }
   }
- 
-  getLibraryId() {
-    this.signService.getLibraryDocumentID()
-      .subscribe(data => {
-        // Get Library Document ID
-        const libraryId = data.libraryId;
-        console.log(libraryId);
-      }),
-        error => {
-          console.log(error);
-        }
-  }
 
+  submitAgreement() {
+    const docName = "[DEMO USE ONLY] Pet Form";
+    this.signService.getLibraryDocuments()
+      .subscribe(data => {
+        let id: string;
+        // Get Library Document ID
+        const doc = data.libraryDocumentList.find(x => x.name === docName);
+        if (doc) {
+          id = doc.id;
+          this.postAgreement(id);
+        }
+        else {
+          console.log("Doc not found");
+        }
+      }),
+      error => {
+        console.log(error);
+      }
+  }
+ 
   submit() {
-    // submit document for signing
     // this.uploadFile();
-    // this.getLibraryId();
-    this.postAgreement();
-    console.log(this.pet);
+    this.submitAgreement();
   }
 }
--- boutique/src/app/services/sign.service.ts
+++ boutique/src/app/services/sign.service.ts
@@ -9,11 +9,15 @@ export class SignService {
     baseUrl = 'https://api.na2.echosign.com';
     uploadUrl = this.baseUrl + '/api/rest/v6/transientDocuments';
     sendUrl = this.baseUrl + '/api/rest/v6/agreements';
-    libraryUrl = this.baseUrl + '/api/rest/v6/libraryDocuments'
-    integrationKey = 'Bearer 3AAABLblqZhCUzZTQu2MvFsn9FbiEexxLzYfCVOK9b9U2HvIBW_9OlYJ-FxZDgnMsih6024_N50ZgKujZC3kt5ccinyfceyVE';
+    libraryUrl = this.baseUrl + '/api/rest/v6/libraryDocuments';
+    integrationKey = 'Bearer 3AAABLblqZhAG7qkayUIOJhNzp2GW-e1PUbHr2byu0rBPCQYaCrnMN0I7zVFg_blSnOpqyTL5xanhWv22b2Vc_H9qURCS4UfB';
+    httpOptions = {
+        headers: new HttpHeaders({
+            'Authorization': this.integrationKey
+        })
+    };
 
-    constructor(private http: HttpClient) {
-    }
+    constructor(private http: HttpClient) {}
 
     uploadFile(file: File): Observable<any> {
         const selectedFile = (<HTMLInputElement>document.getElementById("inputFile")).files;
@@ -25,61 +29,19 @@ export class SignService {
         formData.append('File-Name', fileToLoad.name);
         formData.append('File', fileToLoad);
         formData.append('Mime-Type', fileToLoad.type);
-        console.log(formData);
-        // Access Token obtained from authorization code (code in the request, client id, client secret) 
-        const httpOptions = {
-            headers: new HttpHeaders({
-                'Authorization': this.integrationKey
-            })
-        };
-
-        console.log(httpOptions.headers);
-        return this.http.post(this.uploadUrl, formData, httpOptions);
+ 
+        return this.http.post(this.uploadUrl, formData, this.httpOptions);
     }
 
-    // sendAgreement(documentId: string): Observable<any> {
-    //     const httpOptions = {
-    //         headers: new HttpHeaders({
-    //             'Authorization': this.integrationKey
-    //         })
-    //     };
-
-    //     // Information about agreement
-    //     const agreementInfo = {
-    //         'fileInfos': [{
-    //             //"libraryDocumentId": // user library doc id
-    //             'transientDocumentId': documentId
-    //         }],
-    //         'name': 'AgreementForm',
-    //         'participantSetsInfo': [{
-    //             'memberInfos': [{
-    //                 'email': 'signer@waggyfriendboutique.com'
-    //             }],
-    //             'order': 1,
-    //             'role': 'SIGNER'
-    //         }],
-    //         'signatureType': 'ESIGN',
-    //         'state': 'IN_PROCESS'
-    //     };
-
-    //     return this.http.post(this.sendUrl, agreementInfo, httpOptions);
-    // }
-
-    getLibraryDocumentID(): Observable<any> {
-        return this.http.get(this.libraryUrl);
+    getLibraryDocuments(): Observable<any> {
+        return this.http.get(this.libraryUrl, this.httpOptions);
     }
 
-    postAgreement(pet: Pet): Observable<any> {
-        const httpOptions = {
-            headers: new HttpHeaders({
-                'Authorization': this.integrationKey
-            })
-        };
-
+    postAgreement(pet: Pet, documentId: string): Observable<any> {
         // Information about agreement
         const agreementInfo = {
             'fileInfos': [{
-                'libraryDocumentId': 'CBJCHBCAABAAMB9vP1TTb9xCVRIRNDfzy55sa5VUQ4JQ' 
+                'libraryDocumentId': documentId 
             }],
             'name': 'AgreementForm',
             'participantSetsInfo': [{
@@ -101,6 +63,6 @@ export class SignService {
 
         console.log(agreementInfo);
 
-        return this.http.post(this.sendUrl, agreementInfo, httpOptions);
+        return this.http.post(this.sendUrl, agreementInfo, this.httpOptions);
     }
 }
