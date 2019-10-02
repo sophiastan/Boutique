d38622fa8e8138a9e5a9717dd3f9efb97e72d2cb
Phoumrin Tan <phoumrint@sbcglobal.net>
2019-10-02 00:25:18 -0700

    got promise async await to work, docId made first

--- boutique/src/app/grooming/grooming.component.ts
+++ boutique/src/app/grooming/grooming.component.ts
@@ -13,7 +13,6 @@ export class GroomingComponent implements OnInit {
   docName: string;
   customerEmail: string;
   fileToUpload: File = null;
-  // docId: string;
 
   constructor(private router: Router, private signService: SignService) {
       this.pet = new Pet();
@@ -45,148 +44,162 @@ export class GroomingComponent implements OnInit {
       );
   }
 
-  // // setting library document id
-  // setLibraryDocId() {
+  // Gets library document id
+  async getLibraryDocId(): Promise<string> {
+    let docId = '';
+    try {
+        const data = await this.signService.getLibraryDocuments().toPromise();
+        const doc = data.libraryDocumentList.find(x => x.name === this.docName);
+        if (doc) {
+          console.log('getLibraryDocId: ' + doc.id);
+          docId = doc.id;
+        }
+    } catch (error) {
+      console.log(error);
+    }
+    return docId;
+  }
+
+  // Get agreement id for doc id
+  async getAgreementId(): Promise<string> {
+    let agreementId = '';
+    try {
+      const docId = await this.getLibraryDocId();
+      const data = await this.signService.getAgreementId(this.pet, docId).toPromise();
+      agreementId = data.id;
+      console.log('agreementId: ' + agreementId);
+    } catch (error) {
+      console.log(error);
+    }
+    return agreementId;
+  }
+
+  // Get details of library doc
+  async getLibraryDocumentDetails(): Promise<string> {
+  let documentDetails = '';
+  try {
+    const docId = await this.getLibraryDocId();
+    const data = await this.signService.getLibraryDocumentDetails(docId).toPromise();
+    documentDetails = JSON.stringify(data);
+    console.log('Library Document Details: ' + documentDetails);
+  } catch (error) {
+    console.log(error);
+  }
+  return documentDetails;
+  }
+
+  // getAgreement() {
+  //   // Get the list of library documents
   //   this.signService.getLibraryDocuments()
   //     .subscribe(data => {
+  //       let docId: string;
+
+  //       // Find a document for a document name
   //       const doc = data.libraryDocumentList.find(x => x.name === this.docName);
   //       if (doc) {
-  //         // this.docId = doc.id;
-  //         this.docId = '4';
-  //         console.log("RUNNING setLibraryDocId: " + this.docId);
+  //         docId = doc.id;
+
+  //         // Get agreement id for doc id
+  //         this.signService.getAgreementId(this.pet, docId)
+  //         .subscribe(data => {
+  //           const agreementId = data.id;
+  //           console.log('agreementId: ' + agreementId);
+  //           // console.log('Customer Email: ' + this.customerEmail);
+  //           // console.log('Library ID: ' + docId);
+  //         }),
+  //         error => {
+  //           console.log(error);
+  //         }
+
+  //         // Get details of library doc
+  //         this.signService.getLibraryDocumentDetails(docId)
+  //           .subscribe(data => {
+  //             console.log('Library Document Details: ' + JSON.stringify(data));
+  //           }),
+  //           error => {
+  //             console.log(error);
+  //           }
+
+  //         // // Get audit trail
+  //         // this.signService.getLibraryAuditTrail(docId)
+  //         //   .subscribe(data => {
+  //         //     console.log('Library Document Audit Trail: ' + JSON.stringify(data));
+  //         //   }),
+  //         //   error => {
+  //         //     console.log(error);
+  //         //   }
+
+  //         // Get combined document
+  //         // this.signService.getCombinedDoc(docId)
+  //         //   .subscribe(data => {
+  //         //     const fileReader = new FileReader();
+  //         //     // read file content on file loaded event
+  //         //     fileReader.onload = () => {
+  //         //         let base64 = fileReader.result;
+  //         //         console.log("Combined Document: " + base64);
+  //         //     }
+  //         //     // convert data to base64
+  //         //     fileReader.readAsDataURL(data);
+  //         //   }),
+  //         //   error => {
+  //         //     console.log(error);
+  //         //   }
+
+  //         // Get url of all visible pages of all docs
+  //         this.signService.getUrlVisiblePages(docId)
+  //           .subscribe(data => {
+  //             console.log('URL of Visible Pages: ' + JSON.stringify(data));
+  //           })
+
+  //         // Get IDs of documents
+  //         this.signService.getLibraryIDs(docId)
+  //           .subscribe(data => {
+  //             console.log('Library IDs: ' + JSON.stringify(data));
+  //           }),
+  //           error => {
+  //             console.log(error);
+  //           }
+
+  //           // Get file stream of a doc
+
+  //           // Get image urls of all visible pages of a doc
+
+  //           // Get data entered into the interactive form fields
+
+  //           // Get members info
+  //           this.signService.getMembersInfo(docId)
+  //           .subscribe(data => {
+  //             console.log('Members Info: ' + JSON.stringify(data));
+  //           }),
+  //           error => {
+  //             console.log(error);
+  //           }
+
+  //           // Get events info
+  //           this.signService.getEventsInfo(docId)
+  //           .subscribe(data => {
+  //             console.log('Events Info: ' + JSON.stringify(data));
+  //           }),
+  //           error => {
+  //             console.log(error);
+  //           }
+
+  //           // Get latest note of library doc for the API user
+  //           this.signService.getLatestNote(docId)
+  //             .subscribe(data => {
+  //               console.log('Latest Note: ' + JSON.stringify(data));
+  //             }),
+  //             error => {
+  //               console.log(error);
+  //             }
+  //       }
+  //       // Error if document name is not correct
+  //       else {
+  //         console.log("Error: Document is not found.");
   //       }
   //     }),
   //     error => {
   //       console.log(error);
   //     }
   // }
-
-  // getAgreement() {
-  //   // Get agreement id for doc id
-  //   // this.setLibraryDocId();
-  //   console.log("RUNNING getAgreement: " + this.docId);
-  //   this.signService.getAgreementId(this.pet, this.docId)
-  //   .subscribe(data => {
-  //     const agreementId = data.id;
-  //     console.log('agreementId: ' + agreementId);
-  //   }),
-  //   error => {
-  //     console.log(error);
-  //   }
-  // }
-
-  getAgreement() {
-    // Get the list of library documents
-    this.signService.getLibraryDocuments()
-      .subscribe(data => {
-        let docId: string;
-
-        // Find a document for a document name
-        const doc = data.libraryDocumentList.find(x => x.name === this.docName);
-        if (doc) {
-          docId = doc.id;
-
-          // Get agreement id for doc id
-          this.signService.getAgreementId(this.pet, docId)
-          .subscribe(data => {
-            const agreementId = data.id;
-            console.log('agreementId: ' + agreementId);
-            // console.log('Customer Email: ' + this.customerEmail);
-            // console.log('Library ID: ' + docId);
-          }),
-          error => {
-            console.log(error);
-          }
-
-          // Get details of library doc
-          this.signService.getLibraryDocumentDetails(docId)
-            .subscribe(data => {
-              console.log('Library Document Details: ' + JSON.stringify(data));
-            }),
-            error => {
-              console.log(error);
-            }
-
-          // // Get audit trail
-          // this.signService.getLibraryAuditTrail(docId)
-          //   .subscribe(data => {
-          //     console.log('Library Document Audit Trail: ' + JSON.stringify(data));
-          //   }),
-          //   error => {
-          //     console.log(error);
-          //   }
-
-          // Get combined document
-          // this.signService.getCombinedDoc(docId)
-          //   .subscribe(data => {
-          //     const fileReader = new FileReader();
-          //     // read file content on file loaded event
-          //     fileReader.onload = () => {
-          //         let base64 = fileReader.result;
-          //         console.log("Combined Document: " + base64);
-          //     }
-          //     // convert data to base64
-          //     fileReader.readAsDataURL(data);
-          //   }),
-          //   error => {
-          //     console.log(error);
-          //   }
-
-          // Get url of all visible pages of all docs
-          this.signService.getUrlVisiblePages(docId)
-            .subscribe(data => {
-              console.log('URL of Visible Pages: ' + JSON.stringify(data));
-            })
-
-          // Get IDs of documents
-          this.signService.getLibraryIDs(docId)
-            .subscribe(data => {
-              console.log('Library IDs: ' + JSON.stringify(data));
-            }),
-            error => {
-              console.log(error);
-            }
-
-            // Get file stream of a doc
-
-            // Get image urls of all visible pages of a doc
-
-            // Get data entered into the interactive form fields
-
-            // Get members info
-            this.signService.getMembersInfo(docId)
-            .subscribe(data => {
-              console.log('Members Info: ' + JSON.stringify(data));
-            }),
-            error => {
-              console.log(error);
-            }
-
-            // Get events info
-            this.signService.getEventsInfo(docId)
-            .subscribe(data => {
-              console.log('Events Info: ' + JSON.stringify(data));
-            }),
-            error => {
-              console.log(error);
-            }
-
-            // Get latest note of library doc for the API user
-            this.signService.getLatestNote(docId)
-              .subscribe(data => {
-                console.log('Latest Note: ' + JSON.stringify(data));
-              }),
-              error => {
-                console.log(error);
-              }  
-        }
-        // Error if document name is not correct
-        else {
-          console.log("Error: Document is not found.");
-        }
-      }),
-      error => {
-        console.log(error);
-      }
-  }
 }
